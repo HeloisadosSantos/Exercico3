aa58dc34495eada7bffec812cd453ad0caa02bc2
HeloisadosSantos <heloisadossantos010@gmail.com>
2023-03-19 02:57:09 -0300

    18/03/2023

--- App.java
+++ App.java
@@ -1,8 +1,10 @@
 public class App {
     public static void main(String[] args) {
         Exercicio obj1Exercicio= new Exercicio();
-        // obj1Exercicio.Exercicio4();
-        obj1Exercicio.Exercicio6();
 
+        // obj1Exercicio.Exercicio4();
+        // obj1Exercicio.Exercicio6();
+        // obj1Exercicio.Exercicio7();
+        obj1Exercicio.Exercicio5();
     }
 }
--- Exercicio.java
+++ Exercicio.java
@@ -1,3 +1,4 @@
+import java.lang.invoke.StringConcatFactory;
 import java.util.Scanner;
 
 /**
@@ -21,10 +22,63 @@ public class Exercicio {
      public void Exercicio6(){
         System.out.println("Digite um número");
         int N = sc.nextInt();
-        if (N>1+2) {
-            System.out.println("Esse é um número Impar" +(N*3) );
+        if (N%2==0) {
+            System.out.println("Esse é um número Par" );
         }else{
-            System.out.println("Número Par");
+            System.out.println("Número Impar");
         }
      }
+
+     public void Exercicio7(){
+      System.out.println("Digite sua Nota");
+      int Nota = sc.nextInt();
+      
+      if (Nota>=7){
+         System.out.println("Aprovado");
+      }
+      else if (Nota==6) {
+         System.out.println("Você esta de Recuperação");
+      }
+      else if (Nota<=5){
+         System.out.println("Reprovado");
+      }
+     }
+
+    public void Exercicio3() {
+      System.out.println("Digite uma letra");
+      String Letra = sc.nextLine();
+      String tipoLetra = "";
+      switch (Letra){
+         case "a": tipoLetra = "vogal";
+         break;
+         case "e": tipoLetra = "vogal";
+         break;
+         case "i": tipoLetra = "vogal";
+         break;
+         case "o": tipoLetra = "vogal";
+         break;
+         case "u": tipoLetra = "vogal";
+         break;
+         default: tipoLetra = "Consoante";
+         break;
     }
+      System.out.println( Letra+ "é um" + tipoLetra);
+   }
+
+   public void Exercicio5(){
+      System.out.println("Qual tipo de cliente você se enquadra ? (Vip,Comum,Funcionario)");
+      String Cliente = sc.nextLine();
+      String Desconto = "";
+      switch (Cliente) {
+         case "Vip": Desconto = "-5%";
+            break;
+         case "Funcionario": Desconto = "1";   
+            break;
+         default: Cliente = "0";
+            break;
+      }
+      System.out.println( Cliente+"o valor é:"+Desconto);
+   }
+}
+
+    
